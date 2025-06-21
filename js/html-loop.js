00a0b1c495600a3365bfcf78afbfdff67ef5b376
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-21 02:56:53 -0400

    html-loop exercise

--- js/html-loop.js
+++ js/html-loop.js
@@ -1,1 +1,11 @@
 const main = document.querySelector('main')
+let html = ''
+
+//<div>1</div>
+
+//or i=1; i<=10; i++
+for (let i = 5; i <= 100; i += 5) {
+  html += `<div>${i}</div>`
+}
+
+main.innerHTML = html
