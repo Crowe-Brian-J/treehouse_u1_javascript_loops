c84c712f1c5ff1cf876457358894e520863d541b
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-21 02:05:01 -0400

    while loop exercises

--- js/while.js
+++ js/while.js
@@ -1,3 +1,11 @@
-function getRandomNumber(upper) {
+const getRandomNumber = (upper) => {
   return Math.floor(Math.random() * upper) + 1
 }
+
+let counter = 0
+while (counter < 10) {
+  console.log(`The random number is ${getRandomNumber(10)}.`)
+  counter++
+}
+
+//while loop seems somewhat inefficient because of for loop
